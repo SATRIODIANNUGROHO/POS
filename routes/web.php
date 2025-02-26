4a256286e7de840743cc4a52f2417bb29b5773fa
SATRIODIANNUGROHO <satriodiannugroho@gmail.com>
2025-02-26 13:23:08 +0700

    JS03

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,18 @@ use App\Http\Controllers\BnHController;
 use App\Http\Controllers\HCController;
 use App\Http\Controllers\BKController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/transaction', [TransactionController::class, 'index']);
 
