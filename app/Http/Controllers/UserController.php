e77dbb31339abd0d65064bddd70cc6148f6c24a3
SATRIODIANNUGROHO <satriodiannugroho@gmail.com>
2025-03-09 01:47:09 +0700

    JS04

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,22 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ]
         );
+        $user -> username = 'manager12';
+        
         $user -> save();
-        return view('user', ['data' => $user]);
+
+        $user -> wasChanged(); // True
+        $user -> wasChanged('username'); // True
+        $user -> wasChanged(['username', 'level_id']); // True
+        $user -> wasChanged('nama'); // False
+        $user -> wasChanged(['nama', 'username']); // True
     }
 }
