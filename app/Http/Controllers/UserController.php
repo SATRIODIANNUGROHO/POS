7c724e6cbb0f3d1f99f394485d59b4f24b4130e3
SATRIODIANNUGROHO <satriodiannugroho@gmail.com>
2025-03-10 22:28:03 +0700

    JS04

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,22 +9,46 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
-        $user -> username = 'manager12';
-        
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+    public function tambah() {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request) {
+        UserModel::create([
+            'username' => $request -> username,
+            'nama' => $request -> nama,
+            'password' => Hash::make('$request -> password'),
+            'level_id' => $request -> level_id
+        ]);
+        return redirect('/user');
+    }
+
+    public function ubah($id) {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request) {
+        $user = UserModel::find($id);
+
+        $user -> username = $request -> username;
+        $user -> nama = $request -> nama;
+        $user -> password = Hash::make('$request -> password');
+        $user -> level_id = $request -> level_id;
+
         $user -> save();
 
-        $user -> wasChanged(); // True
-        $user -> wasChanged('username'); // True
-        $user -> wasChanged(['username', 'level_id']); // True
-        $user -> wasChanged('nama'); // False
-        $user -> wasChanged(['nama', 'username']); // True
+        return redirect('/user');
+    }
+
+    public function hapus($id) {
+        $user = UserModel::find($id);
+        $user -> delete();
+
+        return redirect('/user');
     }
 }
