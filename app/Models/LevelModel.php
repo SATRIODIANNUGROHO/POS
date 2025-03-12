8d26db48e824686a14561a5fffd4b28cb7013ef7
SATRIODIANNUGROHO <satriodiannugroho@gmail.com>
2025-03-13 03:51:45 +0700

    JS05

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -4,14 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LevelModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user';       // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
-    
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $table = 'm_level';       // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'level_id';
 }
